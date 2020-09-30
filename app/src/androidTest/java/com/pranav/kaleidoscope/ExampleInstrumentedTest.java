31e52333668612930599c5e68cc3fcfba6c37805
ayushibsg <72129518+ayushibsg@users.noreply.github.com>
2020-10-01 04:40:09 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/pranav/kaleidoscope/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pranav/kaleidoscope/ExampleInstrumentedTest.java
@@ -14,6 +14,9 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+/*
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.*/
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
